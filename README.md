f8d2ff41bd527dec35a852ad09f8e06588762ad3
alejosandrini <alejo.sandrini@gmail.com>
2022-07-03 23:38:42 -0300

    Ultimas modificaciones

--- README.md
+++ README.md
@@ -9,3 +9,8 @@ Repositorio del grupo 1, comisión 4 - ITBANK.
 - Navarro García, Pablo Sebastian
 - Piazza, Santiago Nicolás
 - Sandrini, Alejo
+
+## Consideraciones Sprint 4:
+1. En el código hay ejemplos para probar
+2. El csv que se ingresa debe estar en la carpeta template
+3. Con "py template\listado_chesques.py -h" se ven las aclaraciones
--- template/listado_chesques.py
+++ template/listado_chesques.py
@@ -1,18 +1,13 @@
 import csv
 import argparse
-from datetime import datetime
+from datetime import date, datetime
 
-"""
+# ejemplo1: py template\listado_chesques.py test.csv 40998788 Pantalla depositado -ec aprobado
+# ejemplo2: py template\listado_chesques.py test.csv 40998788 Pantalla depositado -ec rechazado
+# ejemplo3: py template\listado_chesques.py test.csv 40998788 Pantalla emitido
+# ejemplo4: py template\listado_chesques.py test.csv 23665789 Csv emitido
+# ejemplo5: py template\listado_chesques.py test.csv 23665789 paNtaLLa emitido -rf 2021-04-20:2021-06-01
 
-    5) Si el estado del cheque no se pasa, se deberán imprimir los cheques sin filtrar por estado 
-
-    Aclaracion: En la descripción de los campos, falta el campo TIPO, que es un string 
-        que puede tener los siguientes valores  "EMITIDO" o "DEPOSITADO"
-"""
-# ejemplo1: py template\listado_chesques.py test.csv 40998788 Pantalla 123 -ec aprobado
-# ejemplo2: py template\listado_chesques.py test.csv 40998788 Pantalla 123 -ec rechazado
-# ejemplo3: py template\listado_chesques.py test.csv 40998788 Pantalla 123
-# ejemplo4: py template\listado_chesques.py test.csv 23665789 Csv 123
 parser = argparse.ArgumentParser("""Ingresa Nombre del archivo csv, DNI, Salida,
         Tipo de cheque, Estado de cheque(opcional), Rango de fecha(opcional)""")
 parser.add_argument('csv',type=str,help='Ingresa nombre del archivo csv')
@@ -20,14 +15,14 @@ parser.add_argument('dni',type=int,help='Ingresa el dni que quieres filtrar')
 parser.add_argument('salida',type=str,help='Ingresa tipo de salida (pantalla o csv)')
 parser.add_argument('tipocheque',type=str,help='Ingresa el tipo de cheque (emitido o depositado)')
 parser.add_argument('-ec','--estadocheque',type=str,help='Ingresa el estado del cheque (pendiente, aprobado, rechazado)',default=None)
-parser.add_argument('-rf','--rangofecha',type=str,help='Ingresa un rango de fechas con el formato: xx-xx-xxxx:yy-yy-yyyy')
+parser.add_argument('-rf','--rangofecha',type=str,help='Ingresa un rango de fechas con el formato: xxxx-xx-xx:yyyy-yy-yy')
 
 args = parser.parse_args()
 
 def escribir(resultado):
     dt=datetime.now()
     ts=datetime.timestamp(dt)
-    with open(f'{resultado[0][8]}{ts}.csv', 'w', newline='', encoding='utf-8') as f:
+    with open(f'template\{resultado[0][8]}{ts}.csv', 'w', newline='', encoding='utf-8') as f:
         writer = csv.writer(f)
         for cheque in resultado:
             valor = [cheque [3], cheque[5], cheque[6], cheque[7]]
@@ -36,7 +31,14 @@ def escribir(resultado):
 def filtrarCheque(resultado, estadoCheque):
     return filter(lambda cheque: cheque[10]== estadoCheque.upper(), resultado)
 
-def leer(archivo, dni, salida, tipoCheque, estadoCheque, rangoFecha="01-01-0000:31-12-9999"):
+def filtrarPorFecha(resultado, rangoFecha):
+    fechas = rangoFecha.split(':')
+    fechaOrigen = date.fromisoformat(fechas[0])
+    fechaPago = date.fromisoformat(fechas[1])
+    return filter(lambda cheque: date.fromtimestamp(int(cheque[6]))>fechaOrigen and date.fromtimestamp(int(cheque[7]))<fechaPago, resultado)
+
+
+def leer(archivo, dni, salida, tipoCheque, estadoCheque, rangoFecha="1900-01-01:9999-12-31"):
     diccionario = {}
     with open(f"template\{archivo}",'r', newline='', encoding='utf-8') as f:
         reader = csv.reader(f)
@@ -46,6 +48,7 @@ def leer(archivo, dni, salida, tipoCheque, estadoCheque, rangoFecha="01-01-0000:
         resultado = diccionario.get(str(dni))
         if(estadoCheque != None):
             resultado = filtrarCheque(resultado, estadoCheque)
+        resultado = filtrarPorFecha(resultado, rangoFecha)
         imprimirResultados(salida,resultado)
         
 
@@ -74,7 +77,7 @@ def imprimirResultados(salida,resultado):
         for cheque in resultado:
             print(f"""\nNroCheque:{cheque[0]}\nCodBanco:{cheque[1]}\nCodiScurusal:{cheque[2]}
 NroCuentaOrigen:{cheque[3]}\nNroCuentaDestino:{cheque[4]}\nValor:{cheque[5]}
-FechaOrigen:{cheque[6]}\nFechaPago:{cheque[7]}\nDNI:{cheque[8]}
+FechaOrigen:{date.fromtimestamp(float(cheque[6]))}\nFechaPago:{date.fromtimestamp(float(cheque[7]))}\nDNI:{cheque[8]}
 Tipo:{cheque[9]}\nEstado:{cheque[10]}\n=======================""")
 
     elif(salida.upper() == "CSV"):
