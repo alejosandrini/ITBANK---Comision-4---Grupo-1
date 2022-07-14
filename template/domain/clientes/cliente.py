b9074a2f0db6b7039cfb6403df809443174d71cd
alejosandrini <alejo.sandrini@gmail.com>
2022-07-14 16:48:52 -0300

    Creo estructuras y clases sobre clientes

---
+++ template/domain/clientes/cliente.py
@@ -0,0 +1,22 @@
+class Cliente:
+    def __init__(self, nombre, apellido, numero, dni):
+        self.nombre = nombre
+        self.apellido = apellido
+        self.numero = numero
+        self.dni = dni
+
+    def puede_crear_chequera():
+        pass
+    def puede_crear_tarjeta_credito():
+        pass
+    def puede_comprar_dolar():
+        pass
+
+class Classic(Cliente):
+    pass
+
+class Gold(Cliente):
+    pass
+
+class Black(Cliente):
+    pass
