fef45d4a5d938c230d1e4448d520fae2f67cc344
alejosandrini <alejo.sandrini@gmail.com>
2022-07-14 16:54:25 -0300

    Creo clase Direccion y Cuenta

---
+++ template/domain/clientes/cuenta.py
@@ -0,0 +1,7 @@
+class Cuenta:
+    def __init__(self, limite_extraccion_diario, limite_transferencia_recibida, monto, costo_transferencias, saldo_descubierto_disponible):
+        self.limite_extraccion_diario = limite_extraccion_diario
+        self.limite_transferencia_recibida = limite_transferencia_recibida
+        self.monto = monto
+        self.costo_transferencias = costo_transferencias 
+        self.saldo_descubierto_disponible = saldo_descubierto_disponible
